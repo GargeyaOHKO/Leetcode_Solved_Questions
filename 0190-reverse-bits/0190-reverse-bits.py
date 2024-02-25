ae4bd5d983cc855ab82d7d3381fbe2245b102605
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-02-26 01:15:54 +0530

    Time: 21 ms (24.86%), Space: 11.7 MB (44.23%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -4,13 +4,10 @@ class Solution:
     def reverseBits(self, n):
         one=1
         res=0
-        while one<=2**31:    
-            bit=n&one
-            one=one<<1
-            if bit>0:
-                res=res|1
-            else:
-                res=res|0    
-            if one<=2**31:
+        for i in range(32):
+            bit=n&1
+            n=n>>1
+            res=res|bit
+            if i<31:
                 res=res<<1
         return res    
