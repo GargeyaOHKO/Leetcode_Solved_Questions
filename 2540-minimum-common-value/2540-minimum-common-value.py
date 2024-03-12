72f931e6d991220935809abc587964c1b7818953
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-12 16:13:39 +0530

    Time: 385 ms (7.7%), Space: 35.4 MB (85.75%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,22 +1,13 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        d1={}
-        d2={}
-        minnum=9999999999
-        for i in nums1:
-            if i not in d1:
-                d1[i]=1
-            else:
-                d1[i]+=1    
-        for i in nums2:
-            if i not in d2:
-                d2[i]=1
-            else:
-                d2[i]+=1
-        for i in d1:
-            if i in d2:
-                minnum=min(minnum,i)            
-        if minnum==9999999999:
-            return -1
-        return minnum        
+        l1=0
+        l2=0
+        while l1<len(nums1) and l2<len(nums2):
+            if nums1[l1]==nums2[l2]:
+                return nums1[l1]
+            elif nums1[l1]<nums2[l2]:
+                l1+=1
+            elif nums2[l2]<nums1[l1]:
+                l2+=1
+        return -1                    
         
