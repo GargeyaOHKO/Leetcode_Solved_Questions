42d2dcc1778ea049768e34050626c7a2101105a6
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-13 20:36:50 +0530

    Time: 55 ms (38.48%), Space: 16.7 MB (23.99%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        l1=[]
+        l2=[]
+        c=0
+        if n==1:
+            return 1
+        for i in range(1,n+1):
+            c+=i
+            l1.append(c)
+        c=0
+        for i in range(n,0,-1):
+            c+=i
+            l2.insert(0,c) 
+        for i in range(1,len(l1)-1):
+            if l1[i]==l2[i]:
+                return i+1
+        return -1        
+
+        
