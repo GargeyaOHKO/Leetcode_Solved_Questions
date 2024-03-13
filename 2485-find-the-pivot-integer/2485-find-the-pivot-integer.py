2c852c44d751bd67bf511de12421bc69720c4c41
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-13 20:39:38 +0530

    Time: 74 ms (29.07%), Space: 17 MB (10.72%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -8,10 +8,11 @@ class Solution:
         for i in range(1,n+1):
             c+=i
             l1.append(c)
-        c=0
-        for i in range(n,0,-1):
-            c+=i
-            l2.insert(0,c) 
+        for i in range(1,n):
+            l2.append(c) 
+            c-=i
+        print(l1)
+        print(l2)    
         for i in range(1,len(l1)-1):
             if l1[i]==l2[i]:
                 return i+1
