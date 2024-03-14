b39e6eb1233c2f4cd985b7c2bbe806c3489fdbd4
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-14 14:33:43 +0530

    Time: 218 ms (46.53%), Space: 15.4 MB (39.6%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def numSubarraysWithSum(self, nums, goal):
-        def helper(goal):
-            if goal<0:
-                return 0
-            l,count=0,0    
-            c=0
-            for i in range(len(nums)):
-                c+=nums[i]
-                while c>goal:
-                    c-=nums[l]
-                    l+=1
-                count+=(i-l+1)    
-            return count
-        return helper(goal)-helper(goal-1)        
+        d={}
+        c,count=0,0
+        for i in range(len(nums)):
+            c+=nums[i]
+            if c==goal:
+                count+=1           
+            if c-goal in d:
+                count+=d[c-goal]
+            if c not in d:
+                d[c]=1
+            else:
+                d[c]+=1         
+        return count                          
         """
         :type nums: List[int]
         :type goal: int
