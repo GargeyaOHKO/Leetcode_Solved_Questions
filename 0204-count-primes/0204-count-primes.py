cc9dc5279d382361054f6d19c18e194307383cdf
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-19 03:01:07 +0530

    Time: 1253 ms (75.49%), Space: 55 MB (98.23%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,14 +2,11 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n<3:
             return 0
-        l=[0]*(n)
+        l=[1]*(n)
+        l[0]=l[1]=0
         for i in range(2,int(n**0.5)+1):
-            if l[i]==0:
+            if l[i]==1:
                 for j in range(i*i,n,i):
-                    l[j]=1
-        c=0        
-        for i in range(2,n):
-            if l[i]==0:
-                c+=1               
-        return c
+                    l[j]=0            
+        return sum(l)
         
