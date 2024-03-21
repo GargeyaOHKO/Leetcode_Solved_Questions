bfe52e85e1b07d86e8fc1f1fe7f1cf176be462bb
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-21 14:43:54 +0530

    Time: 610 ms (5%), Space: 16.7 MB (19.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,16 +8,13 @@ class Solution:
                 d[s[r]]=1
                 c+=1
                 r+=1
-            elif s[r] in d:    
-                if l<len(s)-1:   
-                    l+=1
-                    r=l
-                    d={}
-                    d[s[r]]=1
-                    c=1
-                    r+=1
-                else:
-                    break               
+            elif s[r] in d and l<len(s)-1:       
+                l+=1
+                r=l
+                d={}
+                d[s[r]]=1
+                c=1
+                r+=1              
             maxc=max(maxc,c)       
         return maxc    
 
