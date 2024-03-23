95e869b13ed1132af6eabce35e4ba8cebeedcfb7
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-23 11:11:41 +0530

    Time: 369 ms (5.54%), Space: 16.8 MB (93.8%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -3,13 +3,12 @@ class Solution:
         for i in range(len(nums)-1):
             if nums[i]==nums[i+1]:
                 nums[i]=nums[i]*2
-                nums[i+1]=0
-        l=len(nums)-1       
+                nums[i+1]=0     
         for i in range(len(nums)):
             if nums[i]==0:
                 j=i
                 while nums[j]==0 and j<len(nums)-1:
                     j+=1
                 nums[j],nums[i]=nums[i],nums[j]               
-        return nums                
+        return nums              
         
