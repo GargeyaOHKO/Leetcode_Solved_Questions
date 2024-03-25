88b27aba923892d2005297db0b326d2e2bc90de2
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-25 18:58:35 +0530

    Time: 39 ms (75.56%), Space: 12.3 MB (17.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,38 +4,18 @@ class Solution(object):
             return False
         d1={}
         d2={}
-        for i in s1:
-            if i not in d1:
-                d1[i]=1
-            else:
-                d1[i]+=1
         for i in range(len(s1)):
-            if s2[i] not in d2:
-                d2[s2[i]]=1
-            else:
-                d2[s2[i]]+=1  
-        if d1==d2:
-            return True        
-
-        l=0
-        r=len(s1)   
-        while r<len(s2):
-            if d1==d2:
-                return True
-            else:
-                if s2[r] not in d2:
-                    d2[s2[r]]=1
-                else:    
-                    d2[s2[r]]+=1
-                d2[s2[l]]-=1
-                if d2[s2[l]]==0:
-                    del d2[s2[l]]
-                if d1==d2:
-                    return True    
-                r+=1
-                l+=1
+            d1[s1[i]]=d1.get(s1[i],0)+1
+            d2[s2[i]]=d2.get(s2[i],0)+1                 
+        if d1==d2: return True       
+        l=0 
+        for r in range(len(s1),len(s2)):
+            d2[s2[r]]=d2.get(s2[r],0)+1
+            d2[s2[l]]-=1
+            if d2[s2[l]]==0: del d2[s2[l]]
+            if d1==d2: return True    
+            l+=1
         return False            
-
         """
         :type s1: str
         :type s2: str
