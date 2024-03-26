6a142528509e4fdb3d2d478c3d6a30ba4776c045
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-03-26 15:55:01 +0530

    Time: 330 ms (13.6%), Space: 22.3 MB (95.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,15 +4,12 @@ class Solution(object):
             if nums[i]<=0:
                 nums.pop(i)
 
-        print(nums)
         for i in range(len(nums)):
-            print(nums[i])
             if (abs(nums[i])-1)>=len(nums):
                 continue
             else:
                 if nums[abs(nums[i])-1]>0:
                     nums[abs(nums[i])-1]=0-nums[abs(nums[i])-1]
-        print(nums)
         c=1
         for i in nums:
             if i>0:
