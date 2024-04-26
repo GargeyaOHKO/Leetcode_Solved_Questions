dd2103a6ceba2f9a1edc8e0f56e39b3c66b78ee8
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-04-26 12:26:22 +0530

    Time: 16 ms (44.47%), Space: 11.7 MB (77.92%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,29 +6,24 @@
 #         self.right = right
 class Solution(object):
     def isSameTree(self, p, q):
-        curr,stack=p,[]
+        if (p==None and q!=None) or (p!=None and q==None):
+            return False
+        curr1,stack1=p,[]
+        curr2,stack2=q,[]
         res1,res2=[],[]
-        while curr or stack:
-            if curr:
-                res1.append(curr.val)
-                stack.append(curr.right)
-                curr=curr.left
+        while curr2 or stack2 or curr1 or stack1:
+            if curr1 or curr2:
+                if (curr1==None and curr2!=None) or (curr1!=None and curr2==None) or curr1.val!=curr2.val:
+                    return False    
+                stack1.append(curr1.right)
+                stack2.append(curr2.right)
+                curr1=curr1.left
+                curr2=curr2.left
             else:
-                res1.append('a')
-                curr=stack.pop()
-        curr,stack=q,[]
-        while curr or stack:
-            if curr:
-                res2.append(curr.val)
-                stack.append(curr.right)
-                curr=curr.left
-            else:
-                res2.append('a')
-                curr=stack.pop()
-        if res1==res2:
-            return True
-        else:
-            return False        
+                curr1=stack1.pop()
+                curr2=stack2.pop()
+        return True
+               
         """
         :type p: TreeNode
         :type q: TreeNode
