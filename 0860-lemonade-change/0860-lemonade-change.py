39415d2ca8e4cb22ada1db9e3d78c1c6be3b418b
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-04-27 13:17:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        d={5:0,10:0,20:0}
+        if bills[0]==10 or bills[0]==20:
+            return False
+        for i in bills:
+            d[i]+=1 
+            if i==10:
+                d[5]-=1
+                if d[5]<0:
+                    return False
+            elif i==20:
+                if d[10]>0:
+                    d[10]-=1
+                    d[5]-=1
+                elif d[10]<0:
+                    d[5]-=3
+                if d[5]<0:
+                    return False       
+        return True                  
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        
