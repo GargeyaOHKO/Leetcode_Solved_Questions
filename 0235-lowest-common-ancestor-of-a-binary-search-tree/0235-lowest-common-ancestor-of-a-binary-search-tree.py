74b34aeb7d18fbbd39acb8b8e2a210bb03d82a90
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-05-01 16:24:43 +0530

    Time: 45 ms (75.91%), Space: 19.7 MB (77.78%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -4,30 +4,28 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         res1,res2=[],[]
-        curr=root
-        flag=True
-        while curr and flag:
-            res1.append(curr.val)
-            if curr.val==p.val:
-                break
-                flag=False
-            elif curr.val>p.val:
-                curr=curr.left
-            else:
-                curr=curr.right
-        curr=root       
-        while curr:
-            res2.append(curr.val)
-            if curr.val==q.val:
-                break
-            elif curr.val>q.val:
-                curr=curr.left
-            else:
-                curr=curr.right   
+        curr1,curr2=root,root
+        flag1,flag2=True,True
+        while (curr1 or curr2) and (flag1 or flag2):
+            if flag1:
+                res1.append(curr1.val)
+                if curr1.val==p.val:
+                    flag1=False
+                elif curr1.val>p.val:
+                    curr1=curr1.left
+                else:
+                    curr1=curr1.right
+            if flag2:
+                res2.append(curr2.val)
+                if curr2.val==q.val:
+                    flag2=False
+                elif curr2.val>q.val:
+                    curr2=curr2.left
+                else:
+                    curr2=curr2.right   
         ans=0
         for i in range(min(len(res1),len(res2))):
             if res1[i]==res2[i]:
