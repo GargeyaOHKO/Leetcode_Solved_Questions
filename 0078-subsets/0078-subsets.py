402754666c27fb499548e21e7f46691fcc6dd3a6
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-05-22 20:17:52 +0530

    Time: 10 ms (94.15%), Space: 11.8 MB (47.03%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,15 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def subsets(self, nums):
         fl=[]
-        for i in range((2**len(nums))):
+        for i in range(2**len(nums)):
             l=[]
             for j in range(len(nums)):
-                if (i&(1<<j))!=0:
+                if i&(1<<j)!=0:
                     l.append(nums[j])
             fl.append(l)
         return fl            
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         
