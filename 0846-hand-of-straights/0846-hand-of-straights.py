dfdfe4579f1c9f2c086f8c63d9823c8cf1574ce4
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-06 18:32:24 +0530

    Time: 1069 ms (7.16%), Space: 18.5 MB (28.91%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -6,25 +6,24 @@ class Solution:
         for i in hand:
             d[i]=d.get(i,0)+1
         d={k:d[k] for k in sorted(d)}
-
+        l=[]
         c=0
         while c!=len(hand):
             k=groupSize
-            l=[]
-            for i in d:
-                if k==0:
-                    break
-                if d[i]!=0:
-                    print(i)    
-                    if l==[]:
-                        l.append(i)
-                    elif l[-1]!=i-1:
-                        return False
-                    else:
-                        l.append(i)    
-                    d[i]-=1
-                    k-=1            
-                    c+=1
+            while k!=0:
+                for i in d:
+                    if k==0:
+                        break
+                    if d[i]!=0: 
+                        if k==groupSize:
+                            l.append(i)
+                        elif l[-1]!=(i-1):
+                            return False
+                        else:
+                            l.append(i)    
+                        d[i]-=1
+                        k-=1            
+                        c+=1
         return True
          
 
