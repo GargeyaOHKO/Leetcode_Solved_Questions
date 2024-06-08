09ff1ba431f3d5e823c0fd36d45ee20872aebcc0
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-08 18:40:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,17 +1,14 @@
 class Solution(object):
     def checkSubarraySum(self, nums, k):
-        if len(nums)<2:
-            return False
         d={0:-1}
         s=0
         for i in range(len(nums)):
-            #print(d)
             s+=nums[i]
             if i==0:
                 d[s%k]=i
             elif s%k not in d: 
                 d[s%k]=i
-            elif (i-d[s%k]>1) or s%k==0:
+            elif (i-d[s%k]>1):
                 return True              
         return False            
         """
