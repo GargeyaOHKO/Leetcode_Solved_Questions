1292df121ed547833deff967e95bbee506e2738a
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-14 13:55:44 +0530

    Time: 40 ms (36.26%), Space: 16.7 MB (57.6%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,58 +7,30 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         l=[]
-        def dfs(root,c):
+        d={}
+        def dfs(root,c,r):
             if not root:
                 return None
             else:
-                l.append([root.val,c])
-                dfs(root.left,c-1)
-                dfs(root.right,c+1)  
-            return None   
-        dfs(root,0)
-        d={}
-        maxi,mini=float("-inf"),float("inf")
-        for i in l:
-            if i[1] not in d:
-                maxi=max(maxi,i[1])
-                mini=min(mini,i[1])
-                d[i[1]]=[i[0]]
-            else:
-                d[i[1]].append(i[0])     
-        #print(d)
-
-        q,d2=deque(),{}
-        q.append(root)
-        c=-1
-        while q:
-            tl=[]
-            c+=1
-            for i in range(len(q)):
-                node=q.popleft()
-                if node:
-                    if node.val not in d2:
-                        d2[node.val]=[c]
-                    else:
-                        d2[node.val].append(c)
-                    q.append(node.left)
-                    q.append(node.right)  
-        #print(d2)
-        l=[]        
-        for i in range(mini,maxi+1):
-            j=0
-            while j<len(d[i])-1:
-            #for j in range(len(d[i])-1):
-                #print(d2)
-                if (d2[d[i][j]][0]==d2[d[i][j+1]][0] and d[i][j]>d[i][j+1]) or d2[d[i][j]][0]>d2[d[i][j+1]][0]:
-                    d[i][j],d[i][j+1]=d[i][j+1],d[i][j]
-                    if len(d2[d[i][j]])>1:
-                        d2[d[i][j]].pop(0)
-                    if len(d2[d[i][j+1]])>1:
-                        d2[d[i][j+1]].pop(0)
-                    j=0
+                if c not in d:
+                    d[c]=[(r,root.val)]
                 else:
-                    j+=1
-            l.append(d[i])        
-        #print(l) 
-        return l                       
+                    d[c].append((r,root.val))
+                dfs(root.left,c-1,r+1)
+                dfs(root.right,c+1,r+1)  
+            return None   
+        dfs(root,0,0)     
+        #print(d)
+        mini=min(d)
+        maxi=max(d)
+        tl,fl,l=[],[],[]       
+        for i in range(mini,maxi+1):
+            l.append(sorted(d[i]))
+        #print(l)    
+        for i in l:
+            tl=[]
+            for j in i:
+                tl.append(j[1])
+            fl.append(tl)    
+        return fl                       
         
