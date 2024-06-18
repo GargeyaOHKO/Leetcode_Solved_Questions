26d3db5736bb1f4c551dfe9287c2c8555cfcfe3c
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-18 23:11:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def combinationSum(self, candidates, target): 
+        import itertools
+        res=[]
+        l=[]
+        def dfs(i):
+            if i>=len(candidates) or sum(l)>target:
+                return None
+            if sum(l)==target:
+                res.append(list(l))
+            l.append(candidates[i])
+            dfs(i)
+            dfs(i+1)
+            l.pop()
+            dfs(i+1)
+            return res
+        dfs(0)
+        return list(res for res,_ in itertools.groupby(res))
+                
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
