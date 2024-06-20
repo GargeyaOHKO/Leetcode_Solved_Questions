6a58effd70ad1d78c5f5025ac8c940eaf38ccfdd
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-21 00:23:26 +0530

    Time: 8 ms (93.87%), Space: 11.6 MB (50.75%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,22 +5,21 @@
 #         self.next = next
 class Solution(object):
     def swapPairs(self, head):
-        curr,c=head,0
+        curr,c,odd=head,0,False
         while curr!=None:
             c+=1
             curr=curr.next
+        if c==1:
+            return head
         l,last=[],head
         if c%2!=0:
             while last.next!=None:
                 last=last.next
-        if c==1:
-            return last
-        l=[]
-        c1=c
-        if c1%2!=0:
-            c1-=1
+        if c%2!=0:
+            odd=True
+            c-=1
         curr,count=head,0
-        while count!=c1:
+        while count!=c:
             prev=None
             for i in range(2):
                 nxt=curr.next
@@ -35,7 +34,7 @@ class Solution(object):
             final.next=i
             while final.next!=None:
                 final=final.next
-        if c%2!=0:
+        if odd:
             final.next=last
         return head.next
         """
