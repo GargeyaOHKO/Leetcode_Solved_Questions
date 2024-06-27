9baafc2baeaa0d0ec35c64131618fcb284e07f1e
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-06-27 12:32:53 +0530

    Time: 238 ms (30.3%), Space: 18 MB (70.68%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -6,12 +6,12 @@ class Solution:
             if abs(x-arr[i])<minabs:
                 minabs=abs(x-arr[i])
                 index=i
-        print(index)
+        #print(index)
         l,r=index,index
         k-=1
         #print(l,r)
         while k>0:
-            print(l,r,k)
+            #print(l,r,k)
             if l-1>=0 and r+1<len(arr) and abs(arr[l-1]-x)<=abs(arr[r+1]-x):
                 l-=1
                 k-=1
@@ -24,9 +24,9 @@ class Solution:
             elif l-1<0 and r+1<len(arr):
                 r+=1
                 k-=1
-            print(l,r,k)
-            print()
-        print(l,r)
+            #print(l,r,k)
+            #print()
+        #print(l,r)
         fl=[]
         for i in range(l,r+1): 
             fl.append(arr[i])
