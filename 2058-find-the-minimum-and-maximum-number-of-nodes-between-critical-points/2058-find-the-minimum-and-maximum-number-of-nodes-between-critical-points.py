66f49f74e140683d1db7f4a48fb1492abf512bfd
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-07-05 12:34:50 +0530

    Time: 920 ms (38.89%), Space: 92.6 MB (22.22%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,35 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        maxi=0
-        mini=float('infinity')
-        curr=head.next
-        prev=head
-        c1=0
-        c2=0
-        firstflag=False
+class Solution(object):
+    def nodesBetweenCriticalPoints(self, head):
+        if head.next.next==None:
+            return [-1,-1]
+        maxi=float('-inf')
+        mini=float('inf')
+        curr=head
+        prev=curr
+        curr=curr.next
+        l=[]
+        c=0
         while curr.next!=None:
+            if c>0:
+                c+=1
             if (curr.val>prev.val and curr.val>curr.next.val) or (curr.val<prev.val and curr.val<curr.next.val):
-                #print(curr.val)
-                if firstflag:
-                    mini=min(mini,c2)
-                    c2=0
-                if not firstflag:
-                    firstflag=True
-                    c1+=1
-                else:
-                    maxi=max(maxi,c1)
-                c2+=1    
-            else:
-                c2+=1
-                c1+=1
+                #print(prev,curr.val)
+                if c==0:
+                    c=1
+                l.append(c)
+            prev=curr
             curr=curr.next
-            prev=prev.next    
-        if maxi==0:
+        #print(l)
+        for i in range(len(l)-1):
+            mini=min(mini,l[i+1]-l[i])
+
+        if len(l)<2:
             return [-1,-1]
         else:
-            return [mini,maxi]            
+            return [mini,l[-1]-l[0]]
+
+        """
+        :type head: Optional[ListNode]
+        :rtype: List[int]
+        """
+        
