3254154ad69caa397af16c1c82cdef7a0870cd23
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-07-07 22:20:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -1,17 +1,20 @@
 class Solution(object):
     def validStrings(self, n):
         res=[]
+        dp={}
         def dfs(i,count,s):
             if len(s)>1 and s[-1]=='0' and s[-1]==s[-2]:
                 return None
             if i==n:
                     res.append(str(s))
                     return None
+            if (i,s) in dp:
+                return dp[(i,s)]
             s+='1'            
-            dfs(i+1,count+1,s)
+            dfs(i+1,s)
             s=s[:-1]
             s+='0'
-            dfs(i+1,count,s)
+            dfs(i+1,s)
             return res
         return dfs(0,0,"") 
         """
