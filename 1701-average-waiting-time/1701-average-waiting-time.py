e153446f853080f32a117b6f2d9de0b5fc27a6c8
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-07-09 13:20:21 +0530

    Time: 771 ms (22.68%), Space: 57.1 MB (45.36%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,21 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        nums=customers[:]
+        l=[]
+        cur=nums[0][0]+nums[0][1]
+        l.append(cur-nums[0][0])
+        for i in range(1,len(nums)):
+            extra=cur-nums[i][0]
+            #print(extra)
+            if extra<0:
+                extra=0
+                cur=nums[i][0]+nums[i][1]
+            else:
+                cur+=nums[i][1] 
+            l.append(extra+nums[i][1])
+            #print(extra,cur,l)
+        s=0
+        #print(l)
+        for i in l:
+            s+=i
+        return float(s/len(nums))
