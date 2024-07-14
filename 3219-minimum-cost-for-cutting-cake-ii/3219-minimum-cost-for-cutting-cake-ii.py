cbf6a88fca1b5cb5bec1c384aaf5e6ecc01b2352
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-07-14 12:56:35 +0530

    Time: 2059 ms (100%), Space: 32.4 MB (100%) - LeetHub

---
+++ 3219-minimum-cost-for-cutting-cake-ii/3219-minimum-cost-for-cutting-cake-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumCost(self, m: int, n: int, hCut: List[int], vCut: List[int]) -> int:
+        vPart=1
+        hPart=1
+        hCut.sort()
+        vCut.sort()
+        c=0
+        while hCut and vCut:
+            if hCut[-1]>vCut[-1]:
+                c+=hCut[-1]*vPart
+                hPart+=1
+                hCut.pop()
+            else:
+                c+=vCut[-1]*hPart
+                vPart+=1
+                vCut.pop()
+        while hCut:
+            c+=hCut[-1]*vPart
+            hPart+=1
+            hCut.pop()
+        while vCut:
+            c+=vCut[-1]*hPart
+            vPart+=1
+            vCut.pop()
+        return c
