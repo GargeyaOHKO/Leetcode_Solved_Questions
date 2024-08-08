b6026187799e0d89b5caad2c1548d2e163b5f02b
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-08-08 18:10:20 +0530

    Time: 1109 ms (18.33%), Space: 28.2 MB (18.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
-        dp={}
-        l=[]
-        def dfs(index,amount,number):
-            if index==0:
-                if amount%coins[index]==0:
-                    return amount//coins[index]
-                return float('inf')
-            if amount<0 or index<0:
-                return float('inf')
-            if (amount,number) in dp:
-                return dp[(amount,number)]
-            tot = float('inf')
-            dp[(amount,number)]=min(pick,notpick)
-            return dp[(amount,number)]
-        num=dfs(0,amount,0)
+        dp={0:0}
+        def dfs(amt):
+            if amt in dp:
+                return dp[amt]
+            mini=float('inf')
+            for c in coins:
+                diff=amt-c
+                if diff<0:
+                    break
+                mini=min(mini,1+dfs(diff))
+            dp[amt]=mini
+            return dp[amt]
+        num=dfs(amount)
         if num==float("inf"):
             return -1
         else:
