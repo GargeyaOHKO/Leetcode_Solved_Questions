0345d27e610f19d7fb37c647e1a252f88bea6f90
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-08-10 18:04:36 +0530

    Time: 44 ms (38.17%), Space: 16.8 MB (54.34%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,8 +1,11 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        avg=math.ceil(len(citations)/2)
+        citations.sort(reverse=True)
+        print(citations)
         c=0
         for i in citations:
-            if i>=avg:
+            if i>c:
                 c+=1
+            else:
+                break
         return c
