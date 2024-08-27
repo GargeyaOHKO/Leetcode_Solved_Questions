43ce5f19aed03eadc0c0bb506d8d53dceb94e153
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-08-27 18:23:05 +0530

    Time: 36 ms (55.24%), Space: 16.5 MB (93.63%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
@@ -16,14 +16,12 @@ class Solution:
         l.append(palin(pref+1,len(n)%2==0))
 
         l.append(int("0"+'9'*(len(n)-1)))
-        print(l)
 
         l.append(int('1'+('0'*(len(n)-1))+'1'))
-        print(l)
 
         if str(int(n)-11)[:]==str(int(n)-11)[::-1]:
             l.append(int(n)-11)
-        print(l)
+
         for i in range(len(l)-1,-1,-1):
             if l[i]==num:
                 l.remove(num)
@@ -36,6 +34,5 @@ class Solution:
                 fl=[l[i]]
             if mindif==abs(int(n)-int(l[i])):
                 fl.append(l[i])
-        print(fl)
         return str(min(fl))
         
