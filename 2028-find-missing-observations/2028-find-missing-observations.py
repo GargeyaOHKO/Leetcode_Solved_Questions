8102bb239779a12b73cff17b2c639d0986cf739a
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-09-05 17:01:54 +0530

    Time: 1104 ms (10.96%), Space: 26.7 MB (74.6%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.py
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -4,9 +4,9 @@ class Solution:
         val=(mean*(len(rolls)+n))-s
         l=[0]*n
         """if (s+n)//(len(rolls)+n)>mean:
-            return []
-        if val>n*6:
             return []"""
+        if val>n*6 or n>val:
+            return []
         for i in range(val):
             l[i%n]+=1
         if l[-1]==0 or l[0]>6:
