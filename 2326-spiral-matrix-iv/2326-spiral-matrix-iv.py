8867e1698b0f9799e9a958ee3a5df4ac6ce91b5a
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-09-09 10:48:01 +0530

    Time: 726 ms (27.39%), Space: 55.9 MB (45.36%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -7,41 +7,37 @@ class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         mat=[[-1 for i in range(n)] for j in range(m)]
         #print(mat)
-        l=[]
+        res=[]
         curr=head
         while curr!=None:
-            l.append(curr.val)
+            res.append(curr.val)
             curr=curr.next
-        if len(l)==1:
-            mat[0][0]=l[0]
+        if len(res)==1:
+            mat[0][0]=res[0]
             return mat
-        a,b,c,d=0,n,m,-1
-        z,i,j=0,0,0
-        while z<len(l):
-            while j+1<b and z<len(l):
-                print(i,j,z)
-                mat[i][j]=l[z]
-                j+=1
-                z+=1
+        #print(mat,l)
+        l,r,t,b=0,n-1,0,m-1
+        z=0
+        while l<=r and t<=b:
+            for i in range(l,r+1):
+                if z<len(res):
+                    mat[t][i]=res[z]
+                    z+=1
+            t+=1
+            for i in range(t,b+1):
+                if z<len(res):
+                    mat[i][r]=res[z]
+                    z+=1
+            r-=1
+            for i in range(r,l-1,-1):
+                if z<len(res):
+                    mat[b][i]=res[z]
+                    z+=1
             b-=1
-            while i+1<c and z<len(l):
-                print(i,j,z)
-                mat[i][j]=l[z]
-                i+=1
-                z+=1
-            c-=1
-            while j-1>d and z<len(l):
-                print(i,j,z)
-                mat[i][j]=l[z]
-                j-=1
-                z+=1
-            d+=1
-            while i-1>a and z<len(l):
-                print(i,j,z)
-                mat[i][j]=l[z]
-                i-=1
-                z+=1
-            a+=1
-        return mat
+            for i in range(b,t-1,-1):
+                if z<len(res):
+                    mat[i][l]=res[z]
+                    z+=1
+            l+=1
 
-                
+        return mat
