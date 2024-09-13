88a1c4dc3ee63e29c17c885a3f63680dc5d50ea9
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-09-13 10:52:20 +0530

    Time: 831 ms (62.91%), Space: 31.2 MB (82.12%) - LeetHub

--- 3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum/3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum.py
+++ 3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum/3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum.py
@@ -1,10 +1,16 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int]) -> int:
-        d={}
-        c=0
-        for i in nums:
-            d[i]=d.get(i,0)+1
-        for i in d:
-            c+=d[i]
-            c+=(d[i]*(d[i]-1))//2
-        return c
+        stack=[(float('inf'),1)]
+        res=len(nums)
+        for i in range(len(nums)):
+            #print(stack)
+            while nums[i]>stack[-1][0]:
+                stack.pop()
+            if nums[i]==stack[-1][0]:
+                c=stack.pop()[1]
+                stack.append((nums[i],c+1))
+                res+=c+1
+            if nums[i]<stack[-1][0]:
+                stack.append((nums[i],0))
+        return res
+
