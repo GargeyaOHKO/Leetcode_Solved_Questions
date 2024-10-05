9df8beb4d32609b5fd4dd2d501622e58e55bd3d3
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-05 18:24:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,23 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):    
-        if len(s2)<len(s1):
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
             return False
-        d1={}
+        
+        d1=Counter(s1)
         d2={}
-        for i in range(len(s1)):
-            d1[s1[i]]=d1.get(s1[i],0)+1
-            d2[s2[i]]=d2.get(s2[i],0)+1                 
-        if d1==d2: return True       
-        l=0 
-        for r in range(len(s1),len(s2)):
-            d2[s2[r]]=d2.get(s2[r],0)+1
-            d2[s2[l]]-=1
-            if d2[s2[l]]==0: del d2[s2[l]]
-            if d1==d2: return True    
-            l+=1
-        return False            
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+        i,j=0,len(s1)-1
+        for z in range(i,j+1):
+            d2[s2[z]]=d2.get(s2[z],0)+1
+        while j<len(s2)-1:
+            #print(d1,d2)
+            if d1==d2:
+                return True
+            else:
+                d2[s2[i]]-=1
+                if d2[s2[i]]==0:
+                    del d2[s2[i]]
+                i+=1
+                j+=1
+                d2[s2[j]]=d2.get(s2[j],0)+1
+        return False
         
