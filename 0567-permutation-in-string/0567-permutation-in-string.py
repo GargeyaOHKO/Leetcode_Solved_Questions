ac7e924037ed06eed8137b5fea1025f40c54ad03
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-05 18:24:34 +0530

    Time: 353 ms (21.76%), Space: 16.8 MB (22.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,8 +8,8 @@ class Solution:
         i,j=0,len(s1)-1
         for z in range(i,j+1):
             d2[s2[z]]=d2.get(s2[z],0)+1
-        while j<len(s2)-1:
-            #print(d1,d2)
+        while j<len(s2):
+            print(d1,d2)
             if d1==d2:
                 return True
             else:
@@ -18,6 +18,7 @@ class Solution:
                     del d2[s2[i]]
                 i+=1
                 j+=1
-                d2[s2[j]]=d2.get(s2[j],0)+1
+                if j<len(s2):
+                    d2[s2[j]]=d2.get(s2[j],0)+1
         return False
         
