aef7cef9655e18ac3e79fc6afbf8287e5017c8e3
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-06 14:21:07 +0530

    Time: 40 ms (22.98%), Space: 16.6 MB (13.04%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -3,61 +3,33 @@ class Solution:
         l1=list(s1.strip().split(" "))
         l2=list(s2.strip().split(" "))
         if len(l1)>len(l2):
-            l2[:],l1[:]=l1[:],l2[:]
+            l1[:],l2[:]=l2[:],l1[:]
         d1=Counter(l1)
         d2=Counter(l2)
+        rem=[]
+        if " ".join(l1)==" ".join(l2[:len(l1)]) or " ".join(l1)==" ".join(l2[len(l2)-len(l1):]):
+            return True
+            return False
         if len(l1)==len(l2):
-            for i in range(len(l2)):
+            for i in range(len(l1)):
                 if l1[i]!=l2[i]:
                     return False
             return True
-        c=0
-        for i in l2:
-            if i in d1:
-                c+=1
-        if c==0:
-            return False
-        for i in l1:
-            if i not in d2:
-                return False
-        c=0
-        f1=False
-        f11=True
-        for i in l2:
-            #print(i)
-            #print(c,f1)
-            if i not in d1 and c==0:
-                c+=1
-            elif c==1 and i in d1 and f1==False:
-                f1=True
-            elif f1==True and i not in d1:
-                f11=False
-                break
-            if i in d1:
-                d1[i]-=1
-                if d1[i]==0:
-                    del d1[i]
-        l2.reverse()
-        print(l2)
-        c=0
-        d1=Counter(l1)
-        f2=False
-        f22=True
-        for i in l2:
-            if i not in d1 and c==0:
-                c+=1
-            elif c==1 and i in d1 and f2==False:
-                f2=True
-            elif f2==True and i not in d1:
-                f22=False
-            if i in d1:
-                d1[i]-=1
-                if d1[i]==0:
-                    del d1[i]
-        if f11 or f22:
-            return True
-        else:
-            return False
-            
 
+        for i in l2:
+            if i not in d1:
+                rem.append(i)
+            else:
+                d1[i]-=1
+                if d1[i]==0:
+                    del d1[i]    
+        if rem==len(l2):
+            return False
+        if " ".join(rem)+" "+" ".join(l1)==" ".join(l2) or " ".join(l1)+" "+" ".join(rem)==" ".join(l2):
+            return True
         
+        for i in range(len(l1)):
+            if " ".join(l1[:i])+" "+" ".join(rem)+" "+" ".join(l1[i:])==" ".join(l2):
+                return True
+        return False
+            
