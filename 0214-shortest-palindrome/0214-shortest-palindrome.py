9f0fe2da0c5534c513ab10515841de0fe9d3a4b3
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-12 15:00:25 +0530

    Time: 3351 ms (5%), Space: 17.4 MB (36.2%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,12 +2,13 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         e=0
         rs=s[::-1]
+        mod=10**9+7
         n1,n2=0,0
+        mul=1
         for i in range(len(s)):
             n1=(n1*29)+(ord(s[i])-96)
-            n2=n2+((ord(rs[-1-i])-96)*(29**i))
-            #print(n1)
-            #print(n2)
+            n2=n2+((ord(rs[-1-i])-96)*mul)
+            mul*=29
             if n1==n2:
                 e=i+1
         return s[e:][::-1]+s
