ea764a5e2b86f8aa6a818145284e50171c3a6476
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-15 19:31:29 +0530

    Time: 39 ms (33.57%), Space: 16.7 MB (18.12%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,47 @@
+class Solution:
+    def fullJustify(self, words: List[str], mwid: int) -> List[str]:
+        c=0
+        l=[]
+        fl=[]
+        i=0
+        while i<len(words):
+            if c==0:
+                c+=len(words[i])
+                l.append(words[i])
+                i+=1
+            elif (c+len(words[i])+1)<=mwid:
+                c+=len(words[i])+1
+                l.append(words[i])
+                i+=1
+            else:
+                #print(l,c)
+                s=""
+                rem=mwid-c
+                if len(l)==1:
+                    s+=l[0]+" "*(mwid-len(l[0]))
+                else:
+                    each=math.ceil(rem/(len(l)-1))
+                    #print("each",each)
+                    rem=rem%(len(l)-1)
+                    if rem==0:
+                        rem=len(l)-1
+                    #print("rem",rem)
+                    #print("rem",rem)
+                    for j in range(0,len(l)):
+                        if j==len(l)-1:
+                            s+=l[j]
+                        elif rem>0:
+                            s+=l[j]+(" "*(each+1))
+                            rem-=1
+                        else:
+                            s+=l[j]+(" "*(each))
+                fl.append(s)
+                c=0
+                l=[]
+            """print()
+            print(l)
+            print(fl)"""
+        s=" ".join(l)
+        s+=" "*(mwid-len(s))
+        fl.append(s)
+        return fl
