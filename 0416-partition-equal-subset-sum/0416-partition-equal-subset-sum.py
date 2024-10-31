b055c58e0c863440945821fa7d9cf0726fce4543
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2024-10-31 21:32:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,30 +1,18 @@
-class Solution(object):
-    def canPartition(self, nums):
-        l=[]
-        total=sum(nums)
-        if total%2!=0:
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        s=sum(nums)
+        if s&1==1:
             return False
-        k=total//2
+        else:
+            s=s//2
         dp={}
-        self.flag=False
-        def dfs(i,s):
-            if s==k:
-                self.flag=True
-            if i==len(nums)-1:
-                return nums[i]
-            if (i,s) in dp:
-                return dp[(i,s)]
-            if self.flag:
+        def dfs(i,c):
+            if (i,c) in dp:
+                return dp[(i,c)]
+            if i==len(nums):
+                return False
+            if c==s:
                 return True
-            take=dfs(i+1,s+nums[i])
-            nottake=dfs(i+1,s)
-            dp[(i,s)]=take+nottake
-            return dp[(i,s)]
-        dfs(0,0)
-        return self.flag
-            
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        
+            dp[(i,c)]=dfs(i+1,c+nums[i])|dfs(i+1,c)
+            return dp[(i,c)]
+        return dfs(0,0)
