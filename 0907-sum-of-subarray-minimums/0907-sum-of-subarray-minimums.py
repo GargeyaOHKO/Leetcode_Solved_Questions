bb55fda9ab3e0f4c7df3094b26d5be44a611e9fe
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-01-22 21:35:58 +0530

    Time: 225 ms (5.15%), Space: 25.8 MB (6.42%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,22 +1,35 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
+        mod=10**9+7
         nsmall=[len(arr)]*len(arr)
         stack=[]
-        m=max(arr)
         for i in range(len(arr)):
-            #print(stack)
-            #print(nsmall)
             while len(stack)>=1 and stack[-1][0]>arr[i]:
                 val,index=stack.pop()
                 nsmall[index]=i
             if len(stack)==0:
                 stack.append([arr[i],i])
-            elif stack and stack[-1][0]<arr[i]:
+            elif stack and stack[-1][0]<=arr[i]:
                 stack.append([arr[i],i])
-            elif arr[i]==m:
-                stack[i]==-1
         #print(nsmall)
+        psmall=[0]*len(arr)
+        stack=[]
+        for i in range(len(arr)):
+            #print(stack)
+            #print(psmall)
+            while stack and stack[-1][0]>arr[i]:
+                val,index=stack.pop()
+            if len(stack)==0:
+                stack.append([arr[i],i])
+            elif stack[-1][0]<=arr[i]:
+                psmall[i]=stack[-1][1]+1
+                stack.append([arr[i],i])
+        #print(psmall)
         ans=0
+        '''for i in range(len(arr)):
+            #print("l",(i+1-psmall[i]),"r",(nsmall[i]-i),arr[i])
+            ans+=(arr[i]*(nsmall[i]-i)*(i+1-psmall[i]))%mod
+            #print(ans)'''
         stack=[]
         for i in range(len(arr)):
             #print(stack)
@@ -25,16 +38,16 @@ class Solution:
                 stack.pop()
             #print(stack)
             if len(stack)==0:
-                ans+=(i+1)*(nsmall[i]-i)*arr[i]
+                ans+=((i+1)*(nsmall[i]-i)*arr[i])%mod
                 stack.append([arr[i],i])
             elif stack[-1][0]>arr[i]:
                 #print(nsmall[i]-i,i-stack[-1][1]+1)
-                ans+=(nsmall[i]-i)*(i-stack[-1][1]+1)*arr[i]
+                ans+=((nsmall[i]-i)*(i-stack[-1][1]+1)*arr[i])%mod
                 stack.append([arr[i],i])
             else:
-                ans+=(nsmall[i]-i)*(i-stack[-1][1])*arr[i]
+                ans+=((nsmall[i]-i)*(i-stack[-1][1])*arr[i])%mod
                 stack.append([arr[i],i])
-        return ans
+        return ans%mod
             
 
             
