477e2229804288b43e8994727cdd68e89d1bd4f1
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-02-12 16:56:19 +0530

    Time: 1485 ms (87.63%), Space: 19.5 MB (6.53%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -2,16 +2,12 @@ class Solution:
     def longestBalanced(self, s: str) -> int:
         mlen=0
         for i in range(len(s)):
-            d={}
+            d,maxf,uniq={},float("-inf"),0
             for j in range(i,len(s)):
+                if s[j] not in d:
+                    uniq+=1
                 d[s[j]]=d.get(s[j],0)+1
-                var,flag=0,False
-                for z in d:
-                    if var==0:
-                        var=d[z]
-                    else:
-                        if var!=d[z]:
-                            flag=True
-                if not flag:
+                maxf=max(maxf,d[s[j]])
+                if maxf*uniq==(j-i+1):
                     mlen=max(mlen,j-i+1)
         return mlen
