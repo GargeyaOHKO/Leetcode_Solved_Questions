95d8d88cf66730b80eea56693c26ba73ae60be3b
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-02-18 02:57:54 +0530

    Time: 96 ms (98.61%), Space: 30.1 MB (84.17%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -3,19 +3,19 @@ class Solution:
         stack=[]
         maxarea=0
         for i in range(len(heights)):
-            #print(stack,maxarea)
             index=-1
-            while stack and heights[i]<stack[-1][1]:
-                index,height=stack.pop()
-                maxarea=max(maxarea,height*(i-index))
-            if not stack or heights[i]>stack[-1][1]:
+            while stack and heights[i]<stack[-1][0]:
+                val,index=stack.pop()
+                maxarea=max(maxarea,(i-index)*val)
+            if not stack or heights[i]>stack[-1][0]:
                 if index!=-1:
-                    stack.append([index,heights[i]])
+                    stack.append([heights[i],index])
                 else:
-                    stack.append([i,heights[i]])
-        #print(stack,maxarea)
-        while stack:
-            index,height=stack.pop()
-            maxarea=max(maxarea,height*(len(heights)-index))
+                    stack.append([heights[i],i])
+        for i in range(len(stack)):
+            val,index=stack.pop()
+            maxarea=max(maxarea,(len(heights)-index)*val)
         return maxarea
+
+            
             
