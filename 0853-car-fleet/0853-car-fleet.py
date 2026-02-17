5196e75195fb456487fc9879e2261e5bb935287a
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-02-18 02:37:13 +0530

    Time: 745 ms (5.05%), Space: 45.9 MB (5.04%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        d=SortedDict()
+        for i in range(len(position)):
+            d[position[i]]=speed[i]
+        index=0
+        for i in d:
+            position[index]=i
+            speed[index]=d[i]
+            index+=1
+        stack=[]
+        for i in range(len(position)):
+            print((target-position[i])/speed[i])
+            time=(target-position[i])/speed[i]
+            while stack and time>=stack[-1]:
+                stack.pop()
+            if not stack or time<stack[-1]:
+                stack.append(time)
+        return len(stack)
