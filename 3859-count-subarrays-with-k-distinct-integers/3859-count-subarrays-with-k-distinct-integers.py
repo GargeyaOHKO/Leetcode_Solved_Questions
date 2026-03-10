66b1d3435c4cfdc7093e8cbc2523ca1187c3143d
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-03-10 16:30:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3859-count-subarrays-with-k-distinct-integers/3859-count-subarrays-with-k-distinct-integers.py
+++ 3859-count-subarrays-with-k-distinct-integers/3859-count-subarrays-with-k-distinct-integers.py
@@ -5,10 +5,9 @@ class Solution:
         r1,r2=-1,-1
         totalc=0
         for l in range(len(nums)):
-            print(l,r1,r2)
+            #print(l,r1,r2)
             while r2<len(nums) and len(d2)<(k+1):
                 r2+=1
-                print(r2)
                 if r2<len(nums):
                     d2[nums[r2]]=d2.get(nums[r2],0)+1
             while r1<len(nums) and totalnum<k:
