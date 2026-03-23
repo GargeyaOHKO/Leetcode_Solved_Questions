71d9435dd08bd8de911762687ea0522342d80398
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-03-23 23:04:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
         self.maxprod=float('-inf')
-        @lru_cache(None)
+        dp={}
         def dfs(r,c,res):
             if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]):
-                return None 
+                return -1
             if r==len(grid)-1 and c==len(grid[0])-1:
                 self.maxprod=max(self.maxprod,res*grid[r][c])
+            if (r,c,res) in dp:
+                return dp[(r,c,res)]
             #print(r,c,res)
-            dfs(r,c+1,res*grid[r][c])
-            dfs(r+1,c,res*grid[r][c])
+            dp[(r,c,res)]=max(dfs(r,c+1,res*grid[r][c]),dfs(r+1,c,res*grid[r][c]))
+            return dp[(r,c,res)]
         dfs(0,0,1)
         if self.maxprod<0:
             return -1
