f921f32ddbd109a5a20f6d2c9d15aa1f91f10784
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-04-08 01:15:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,39 +2,36 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         po,ao=set(),set()
         moves=[[0,1],[1,0],[0,-1],[-1,0]]
+        q1,q2=deque(),deque()
+        seen1,seen2=set(),set()
         for i in range(len(heights)):
             for j in range(len(heights[0])):
                 if i==0 or j==0:
-                    q=deque()
-                    seen=set()
-                    q.append([i,j])
-                    seen.add((i,j))
+                    q1.append([i,j])
+                    seen1.add((i,j))
                     po.add((i,j))
-                    while q:
-                        for k in range(len(q)):
-                            curr=q.popleft()
-                            for m in moves:
-                                newr,newc=curr[0]+m[0],curr[1]+m[1]
-                                if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
-                                    q.append([newr,newc])
-                                    seen.add((newr,newc))
-                                    po.add((newr,newc))
                 if i==len(heights)-1 or j==len(heights[0])-1:
-                    q=deque()
-                    seen=set()
-                    q.append([i,j])
-                    seen.add((i,j))
+                    q2.append([i,j])
+                    seen2.add((i,j))
                     ao.add((i,j))
-                    while q:
-                        for k in range(len(q)):
-                            curr=q.popleft()
-                            for m in moves:
-                                newr,newc=curr[0]+m[0],curr[1]+m[1]
-                                if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
-                                    q.append([newr,newc])
-                                    seen.add((newr,newc))
-                                    ao.add((newr,newc))
-        #print(ao,po)
+        while q1:
+            for k in range(len(q1)):
+                curr=q1.popleft()
+                for m in moves:
+                    newr,newc=curr[0]+m[0],curr[1]+m[1]
+                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen1) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
+                        q1.append([newr,newc])
+                        seen1.add((newr,newc))
+                        po.add((newr,newc))
+        while q2:
+            for k in range(len(q2)):
+                curr=q2.popleft()
+                for m in moves:
+                    newr,newc=curr[0]+m[0],curr[1]+m[1]
+                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen2) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
+                        q2.append([newr,newc])
+                        seen2.add((newr,newc))
+                        ao.add((newr,newc))
         l=[]
         for i in ao:
             if i in po:
