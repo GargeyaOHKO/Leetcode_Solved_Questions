87f3563883a2538dfbf4423932b47eb9e78f7697
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-04-08 01:25:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,6 +1,5 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        po,ao=set(),set()
         moves=[[0,1],[1,0],[0,-1],[-1,0]]
         q1,q2=deque(),deque()
         seen1,seen2=set(),set()
@@ -9,11 +8,9 @@ class Solution:
                 if i==0 or j==0:
                     q1.append([i,j])
                     seen1.add((i,j))
-                    po.add((i,j))
                 if i==len(heights)-1 or j==len(heights[0])-1:
                     q2.append([i,j])
                     seen2.add((i,j))
-                    ao.add((i,j))
         while q1:
             for k in range(len(q1)):
                 curr=q1.popleft()
@@ -22,7 +19,6 @@ class Solution:
                     if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen1) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
                         q1.append([newr,newc])
                         seen1.add((newr,newc))
-                        po.add((newr,newc))
         while q2:
             for k in range(len(q2)):
                 curr=q2.popleft()
@@ -31,9 +27,8 @@ class Solution:
                     if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and ((newr,newc) not in seen2) and heights[newr][newc]>=heights[curr[0]][curr[1]]:
                         q2.append([newr,newc])
                         seen2.add((newr,newc))
-                        ao.add((newr,newc))
         l=[]
-        for i in ao:
-            if i in po:
+        for i in seen1:
+            if i in seen2:
                 l.append([i[0],i[1]])
         return l
