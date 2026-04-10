6e7c62956fb5a21cfc2442e12a5a85a39ce90dae
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-04-10 21:01:11 +0530

    Time: 126 ms (5.02%), Space: 19.9 MB (43.23%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        for z in range(len(graph)):
+            odd,even=set(),set()
+            q=deque()
+            q.append(z)
+            odd.add(z)
+            k=1
+            while q:
+                print(q,k,odd,even)
+                for i in range(len(q)):
+                    curr=q.popleft()
+                    for j in graph[curr]:
+                        #q.append(j)
+                        if k%2==0:
+                            if j in even:
+                                return False
+                            elif j not in odd:
+                                odd.add(j)
+                                q.append(j)
+                        else:
+                            if j in odd:
+                                return False
+                            elif j not in even:
+                                even.add(j)
+                                q.append(j)
+                k+=1
+        return True
+                        
+
+
+
+
+
