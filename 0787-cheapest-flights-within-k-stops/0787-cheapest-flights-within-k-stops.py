796dbc054b8f2a03afec51bae69b2950c316867f
GargeyaOHKO <111445978+GargeyaOHKO@users.noreply.github.com>
2026-05-11 13:53:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,26 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        d={}
-        for i in range(n+1):
-            d[i]=[]
+        d=defaultdict(list)
         for i in flights:
             d[i[0]].append([i[1],i[2]])
-        heap=[(0,src,0)]
-        best={}
         #print(d)
-        while heap:
-            cost,curr,hop=heapq.heappop(heap)
-            #print(cost,curr,hop)
+        h=[[0,src,0]]
+        seen={}
+        while h:
+            #print(h,mini,seen)
+            cost,curr,hop=heapq.heappop(h)
             if hop>k+1:
                 continue
-            if curr==dst:
+            if curr==dst and hop<=(k+1):
                 return cost
-            if (curr,hop) in best and best[(curr,hop)]<=cost:
+            if (curr,hop) in seen and seen[(curr,hop)]<=cost:
                 continue
-            else:
-                best[(curr,hop)]=cost
-            for nei,price in d[curr]:
-                heapq.heappush(heap,(price+cost,nei,hop+1))
+            seen[(curr,hop)]=cost
+            for j in d[curr]:
+                heapq.heappush(h,[cost+j[1], j[0], hop+1])
         return -1
-            
-        
+
